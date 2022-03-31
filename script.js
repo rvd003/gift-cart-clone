f6d5b606c3d301bae5c6332fe538a6131a44bffe
HemanthKumar-CN <hemanth.html+github@gmail.com>
2022-03-31 17:36:45 +0530

    final index

--- script.js
+++ script.js
@@ -127,3 +127,23 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+// ------------------------------------------
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector(".redMenu_container");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
